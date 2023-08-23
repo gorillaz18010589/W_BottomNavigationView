2828a6a18c9c23cca22d36b3c077864aec57bcf0
hank <gorillaz1801058@gmail.com>
2023-08-23 17:22:12 +0800

    v2. feat: 新增字型Style,取消水波紋及特效處理,navController切換頁面

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,80 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView navView;
+    private static final SparseIntArray NAV_TAB_DIRECTIONS = new SparseIntArray();
+
+    static {
+        NAV_TAB_DIRECTIONS.put(R.id.navigation_home, R.id.homeFragment);
+        NAV_TAB_DIRECTIONS.put(R.id.navigation_dashboard, R.id.dashFragment);
+        NAV_TAB_DIRECTIONS.put(R.id.navigation_notifications, R.id.notificationsFragment);
+        NAV_TAB_DIRECTIONS.put(R.id.navigation_search, R.id.searchFragment);
+        NAV_TAB_DIRECTIONS.put(R.id.navigation_account, R.id.accountFragment);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navView = findViewById(R.id.nav_view);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.homeFragment,
+                R.id.dashFragment,
+                R.id.notificationsFragment,
+                R.id.searchFragment,
+                R.id.accountFragment)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        setBottomNavigationView();
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                int currentId = NAV_TAB_DIRECTIONS.get(id);
+                if (navController.getCurrentDestination().getId() != currentId) {
+                    navController.popBackStack();
+                    navController.navigate(currentId);
+                    navView.setItemTextAppearanceActive(R.style.bottomTitleTextSelected);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
+
+    private void setBottomNavigationView() {
+        navView.setItemTextAppearanceInactive(R.style.bottomTitleTextDefault);
+        navView.setItemTextAppearanceActive(R.style.bottomTitleTextSelected);
+        navView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+
+    }
+
 
 }
---
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.ui.account;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.ui.dashboard.DashboardViewModel;
+
+
+public class AccountFragment extends Fragment {
+    private AccountViewModel accountViewModel;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        accountViewModel =
+                ViewModelProviders.of(this).get(AccountViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_account, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        accountViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
